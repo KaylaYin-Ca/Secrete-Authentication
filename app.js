2535f037806509502ebdbd13a81ebf4088e11c27
YIN <yin@YINdeMacBook-Air.local>
2022-06-03 16:54:07 +0800

    using hash function to encryt password (npm package: md5)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParer = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 const app = express();
 
 // console.log(process.env.API_KEY);
@@ -24,10 +25,6 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
-
 const User = mongoose.model("User",userSchema);
 
 //============Req & Res===============
@@ -41,7 +38,7 @@ app.get("/login",function(req,res){
 
 app.post("/login",function(req,res){
   const userName = req.body.username;
-  const userPassword = req.body.password;
+  const userPassword = md5(req.body.password);
   User.findOne({
     email:userName
   },function(err,foundUser){
@@ -66,7 +63,7 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
   const userName = req.body.username;
-  const userPassword = req.body.password;
+  const userPassword = md5(req.body.password);
 
   User.findOne({
     email:userName,
